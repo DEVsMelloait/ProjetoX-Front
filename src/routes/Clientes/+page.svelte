1c8500f0d64c6423dba666522094f85ab1d8841c
João Caamano <joaocaamano2003@gmail.com>
2023-09-07 12:54:58 -0300

    modals

--- src/routes/Clientes/+page.svelte
+++ src/routes/Clientes/+page.svelte
@@ -1,32 +1,94 @@
- <script>
+<script>
+  import Table from "$lib/Componentes/Organismos/Table.svelte";
 
-import { Button, Modal, Label, Input, Checkbox } from 'flowbite-svelte';
-    let formModal = false;
-    let edit = false;
-    let acao;
+  import { Button, Modal, Label, Input, Checkbox } from "flowbite-svelte";
+  let formModal = false;
+  let edit = false;
 
-    
- </script>
+  const Header = [
+    { label: "Nome", key: "label" },
+    { label: "Email", key: "email" },
+    { label: "Preço", key: "preco" },
+    { label: "Ações", action: true },
+  ];
+  const item = [
+    {
+      label: "juju",
+      categoria: "teste",
+      email: "email@email.com",
+      preco: "123,00",
+    },
+    {
+      label: "juju",
+      categoria: "teste",
+      email: "email@email.com",
+      preco: "123,00",
+    },
+    {
+      label: "juju",
+      categoria: "teste",
+      email: "email@email.com",
+      preco: "123,00",
+    },
+    {
+      label: "juju",
+      categoria: "teste",
+      email: "email@email.com",
+      preco: "123,00",
+    },
+    {
+      label: "juju",
+      categoria: "teste",
+      email: "email@email.com",
+      preco: "123,00",
+    },
+  ];
+</script>
 
 <div class="p-8">
-    <Button on:click={() => (formModal = true)}>Novo Cliente</Button>
 
-    <Modal bind:open={formModal} size="md" autoclose={false} class="w-full">
-        <form class="flex flex-col space-y-6" action="#">
-          <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">{edit == true ? "Editar" : "Adicionar"} Cliente</h3>
-          <Label class="space-y-2">
-            <span>Nome</span>
-            <Input type="text" name="name" placeholder="Nome" required />
-          </Label>
-          <Label class="space-y-2">
-            <span>Email</span>
-            <Input type="email" name="email" placeholder="nome@empresa.com" required />
-          </Label> 
-          <Label class="space-y-2">
-            <span>Telefone</span>
-            <Input type="text" name="phone" placeholder="(xx) xxxxx-xxxx" required />
-          </Label>
-          <Button type="submit" class="w-full1">{edit == true ? "Editar" : "Adicionar"}</Button>
-        </form>
-      </Modal>
+  <Table {Header} data={item} />
+  <div style=" text-align: end; margin-top: 15px; ">
+    <Button on:click={() => (formModal = true)}>Novo Cliente</Button>
+  </div>
+
+  <!-- Modal -->
+  <Modal bind:open={formModal} size="md" autoclose={false} class="w-full">
+    <form class="flex flex-col space-y-6" action="#">
+      <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+        {edit == true ? "Editar" : "Adicionar"} Cliente
+      </h3>
+      <Label class="space-y-2">
+        <span>Nome</span>
+        <Input type="text" name="name" placeholder="Nome" required />
+      </Label>
+      <Label class="space-y-2">
+        <span>Email</span>
+        <Input type="email" name="email" placeholder="nome@empresa.com" />
+      </Label>
+      <Label class="space-y-2">
+        <span>Telefone</span>
+        <Input type="number" name="phone" placeholder="(xx) xxxxx-xxxx" />
+      </Label>
+      <Label class="space-y-2">
+        <span>Rua</span>
+        <Input type="text" name="rua" placeholder="Rua exemplo do exemplo" />
+      </Label>
+      <Label class="space-y-2">
+        <span>Bairro</span>
+        <Input type="text" name="bairro" placeholder="exemplo" />
+      </Label>
+      <Label class="space-y-2">
+        <span>Numero</span>
+        <Input type="email" name="numero" placeholder="132" />
+      </Label>
+      <Label class="space-y-2">
+        <span>Complemento</span>
+        <Input type="text" name="complemento" placeholder="apt. 132" />
+      </Label>
+      <Button type="submit" class="w-full1"
+        >{edit == true ? "Editar" : "Adicionar"}</Button
+      >
+    </form>
+  </Modal>
 </div>
--- src/routes/Produtos/+page.svelte
+++ src/routes/Produtos/+page.svelte
@@ -1,22 +1,21 @@
 <script>
     import Table from "$lib/Componentes/Organismos/Table.svelte";
+    import { Button, Modal, Label, Input, Checkbox } from "flowbite-svelte";
+    let formModal = false;
+    let edit = false;
 
     const Header = [
-        { label: "Nome", key: "label" },
-        { label: "Cor", key: "cor" },
+        { label: "Nome", key: "label" }, 
         { label: "Categoria", key: "categoria" },
         { label: "Preço", key: "preco" },
         { label: "Ações", action: true },
     ];
     const item = [
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
-        { label: "juju", cor: "azul", categoria: "teste", preco: "123,00" },
+        { label: "juju", categoria: "teste", preco: "123,00" },
+        { label: "juju", categoria: "teste", preco: "123,00" }, 
+        { label: "juju", categoria: "teste", preco: "123,00" }, 
+        { label: "juju", categoria: "teste", preco: "123,00" }, 
+        { label: "juju", categoria: "teste", preco: "123,00" }, 
     ];
 </script>
 
@@ -24,6 +23,31 @@
    
 
     <Table {Header} data={item} />
- 
+    <div style=" text-align: end; margin-top: 15px; ">
+        <Button on:click={() => (formModal = true)}>Novo Produto</Button>
+      </div>
     
+  <!-- Modal -->
+  <Modal bind:open={formModal} size="md" autoclose={false} class="w-full">
+    <form class="flex flex-col space-y-6" action="#">
+      <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+        {edit == true ? "Editar" : "Adicionar"} Cliente
+      </h3>
+      <Label class="space-y-2">
+        <span>Nome</span>
+        <Input type="text" name="name" placeholder="Nome" required />
+      </Label>
+      <Label class="space-y-2">
+        <span>Categoria</span>
+        <Input type="text" name="categoria" placeholder="Descartavel" required />
+      </Label>
+      <Label class="space-y-2">
+        <span>Categoria</span>
+        <Input type="number" name="preco" placeholder=10,00 required />
+      </Label>
+      <Button type="submit" class="w-full1"
+        >{edit == true ? "Editar" : "Adicionar"}</Button
+      >
+    </form>
+  </Modal>
 </div>
